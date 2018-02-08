f1435462fdd7fe80c0c881afbe2e0a44e8ee0594
Horang <david.lespine@epita.fr>
2018-02-09 00:31:24 +0100

    Add menu and start game button

---
+++ Assets/MenuButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour {
+
+    //called when button clicked
+	public void buttonStartGame()
+    {
+        SceneManager.LoadScene("Level");
+    }
+}
