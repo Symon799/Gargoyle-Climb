b6f13500e6bd4bb4748af9d3fa529429e125e8d2
Horang <david.lespine@epita.fr>
2018-02-09 15:35:49 +0100

    Add exit button

--- Assets/MenuButton.cs
+++ Assets/MenuButton.cs
@@ -5,9 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class MenuButton : MonoBehaviour {
 
-    //called when button clicked
+    //called when start button clicked
 	public void buttonStartGame()
     {
         SceneManager.LoadScene("Level");
+    }
+
+    //called when exit button clicked
+    public void quit()
+    {
+        Debug.Log("Player has quit game");
+        Application.Quit();
     }
 }
